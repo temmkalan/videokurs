4eab0addabdc53e3521aab5636d61d2bf0b7a601
Рабочий <temmkalan@mail.ru>
2021-07-23 15:13:40 +0300

    Добавление записи в БД в модели

--- news/views.py
+++ news/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import News
+
 def index(request):
-    print(request)
-    return HttpResponse('Страница новостей')
+    news = News.objects.all()
+    res = '<h1>Список новостей<h1>'
+    for item in news:
+        res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
+    #print(request)
+    return HttpResponse(res)
 
 def news2(request):
     print(request)
